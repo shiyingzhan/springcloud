08d8bc21101c69778e1c86ec7330fd2f7d2a5cea
shiyingzhan <shiyingzhan@111.com.cn>
2017-11-24 19:36:03 +0800

    uo

--- eureka/springcloud-config-client/src/main/java/org/springcloud/config/client/controller/SpringCloudConfigClientTest.java
+++ eureka/springcloud-config-client/src/main/java/org/springcloud/config/client/controller/SpringCloudConfigClientTest.java
@@ -1,4 +1,4 @@
-package org.springcloud.config.client.controllet;
+package org.springcloud.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
