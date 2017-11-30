f4917c7a0eb379fecb45abdbc091d78c54e79626
shiyingzhan <shiyingzhan@111.com.cn>
2017-11-30 18:34:49 +0800

    update

--- eureka/springcloud-config-client/src/main/java/org/springcloud/config/client/SpringcloudeConfigClientApp.java
+++ eureka/springcloud-config-client/src/main/java/org/springcloud/config/client/SpringcloudeConfigClientApp.java
@@ -2,11 +2,13 @@ package org.springcloud.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 微服务配置客户端
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringcloudeConfigClientApp 
 {
     public static void main( String[] args )
